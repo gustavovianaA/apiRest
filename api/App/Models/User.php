42bfb13f45c2b92cbcad2544651ba3ebf0948185
Gustavo <gustavow3dev@gmail.com>
2021-02-22 21:19:56 -0300

    criar e atualizar

--- api/App/Models/User.php
+++ api/App/Models/User.php
@@ -1,34 +1,92 @@
 <?php
-    namespace App\Models;
 
-    class User
+namespace App\Models;
+
+class User
+{
+    private static $table = 'user';
+
+    private $conn;
+
+    public function __construct()
     {
-        private static $table = 'user';
+        $this->conn = new \PDO(DBDRIVE . ': host=' . DBHOST . '; dbname=' . DBNAME, DBUSER, DBPASS);
+    }
 
-        public static function select(int $id){
-            $connPdo = new \PDO(DBDRIVE.': host='.DBHOST.'; dbname='.DBNAME, DBUSER, DBPASS);
-            $sql = 'SELECT * FROM ' . self::$table . ' WHERE id = :id';
-            $stmt = $connPdo->prepare($sql);
-            $stmt->bindValue(':id',$id);
-            $stmt->execute();
+    public function select(int $id)
+    {
+        $sql = 'SELECT * FROM ' . self::$table . ' WHERE id = :id';
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(':id', $id);
+        $stmt->execute();
 
-            if($stmt->rowCount() > 0){
-                return $stmt->fetch(\PDO::FETCH_ASSOC);
-            }else{
-                throw new \Exception("Nenhum usuário encontrado!");
-            }
-        }
-
-        public static function selectALL(){
-            $connPdo = new \PDO(DBDRIVE.': host='.DBHOST.'; dbname='.DBNAME, DBUSER, DBPASS);
-            $sql = 'SELECT * FROM ' . self::$table;
-            $stmt = $connPdo->prepare($sql);
-            $stmt->execute();
-
-            if($stmt->rowCount() > 0){
-                return $stmt->fetchAll(\PDO::FETCH_ASSOC);
-            }else{
-                throw new \Exception("Nenhum usuário encontrado!");
-            }
+        if ($stmt->rowCount() > 0) {
+            return $stmt->fetch(\PDO::FETCH_ASSOC);
+        } else {
+            throw new \Exception("Nenhum usuário encontrado!");
         }
     }
+
+    public function selectALL()
+    {
+        $connPdo = new \PDO(DBDRIVE . ': host=' . DBHOST . '; dbname=' . DBNAME, DBUSER, DBPASS);
+        $sql = 'SELECT * FROM ' . self::$table;
+        $stmt = $this->conn->prepare($sql);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            return $stmt->fetchAll(\PDO::FETCH_ASSOC);
+        } else {
+            throw new \Exception("Nenhum usuário encontrado!");
+        }
+    }
+
+    public function insert($data)
+    {
+        $sql = 'INSERT INTO ' . self::$table . ' (email,password,name) VALUES (:email,:password,:name)';
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(':email', $data['email']);
+        $stmt->bindValue(':password', $data['password']);
+        $stmt->bindValue(':name', $data['name']);
+        $stmt->execute();
+
+        if ($stmt->rowCount() > 0) {
+            return 'Usuário(a) inserido com sucesso!';
+        } else {
+            throw new \Exception("Falha ao inserir usuário(a)");
+        }
+    }
+
+    public function update($data)
+    {
+        $sql = 'INSERT INTO ' . self::$table . ' (email,password,name) VALUES (:email,:password,:name)';
+        $sql = 'UPDATE ' . self::$table . ' SET email = :email, password = :password, name = :name WHERE id = :id';
+        $stmt = $this->conn->prepare($sql);
+        $stmt->bindValue(':id', $data['id']);
+        $stmt->bindValue(':email', $data['email']);
+        $stmt->bindValue(':password', $data['password']);
+        $stmt->bindValue(':name', $data['name']);
+ 
+        if ($stmt->execute()) {
+            return 'Usuário(a) alterado com sucesso!';
+        } else {
+            throw new \Exception("Falha ao atualizar usuário(a)");
+        }
+    }
+
+    /*public static function delete($data){
+            $connPdo = new \PDO(DBDRIVE.': host='.DBHOST.'; dbname='.DBNAME, DBUSER, DBPASS);
+            $sql = 'INSERT INTO ' . self::$table . ' (email,password,name) VALUES (:email,:password,:name)';
+            $stmt = $connPdo->prepare($sql);
+            $stmt->bindValue(':email',$data['email']);
+            $stmt->bindValue(':password',$data['password']);
+            $stmt->bindValue(':name',$data['name']);
+            $stmt->execute();
+
+            if($stmt->rowCount() > 0){
+                return 'Usuário(a) inserido com sucesso!';
+            }else{
+                throw new \Exception("Falha ao inserir usuário(a)");
+            }
+        }*/
+}
--- api/App/Services/UserService.php
+++ api/App/Services/UserService.php
@@ -1,31 +1,45 @@
 <?php
-    namespace App\Services;
 
-    use App\Models\User;
+namespace App\Services;
 
-    class UserService
+use App\Models\User;
+
+class UserService
+{
+    private $user;
+    private $userData;
+
+    public function __construct()
     {
-        public function get($id = null)
-        {
-            if($id){
-                return User::select($id);
-            }else{
-                return User::selectAll();
-            }
-        }
+        $this->user = new User;
+        $this->userData = $_POST;
+    }
 
-        public function post()
-        {
-
-        }
-
-        public function update()
-        {
-
-        }
-
-        public function delete()
-        {
-
+    public function get($id = null)
+    {
+        if ($id) {
+            return $this->user->select($id);
+        } else {
+            return $this->user->selectAll();
         }
     }
+
+    public function post()
+    {
+        extract($this->userData);
+        if (isset($id)) {
+            $this->update();
+        } else {
+            return $this->user->insert($this->userData);
+        }
+    }
+
+    public function update()
+    {
+        return $this->user->update($this->userData);
+    }
+
+    public function delete()
+    {
+    }
+}
